c7bc8c03735f99c3610c71abb85c8cbd2375ae10
wnvd <waninaveed21@gmail.com>
2023-12-09 22:07:34 +0530

    made few changes

--- wezterm.lua
+++ wezterm.lua
@@ -2,14 +2,14 @@ local wezterm = require 'wezterm'
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
-config.color_schemes = {
-	['Batman'] = {
-		background = 'black'
-	}
-}
+config.initial_cols = 70
+config.initial_rows = 20
+
+config.line_height = 1.0 -- below 1.0 makes some font smuggy
+
 
 config.window_decorations = 'NONE'
 config.window_background_opacity = 1.0
@@ -17,23 +17,22 @@ config.window_background_opacity = 1.0
 config.use_fancy_tab_bar = false
 
 config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
-
-config.font_size = 15.0
+config.font_size = 18.0
+-- Some 'mono' fonts make icons in terminal look smaller.
 config.font = wezterm.font(
-	'SauceCodePro Nerd Font Mono',
---	'IntoneMono Nerd Font Mono',
---	'BlexMono Nerd Font Mono',
-	{
-		weight = 'Regular',
-		italic = false,
-	}
+    'Courier Prime Code', -- CodeNewRoman Nerd Font, SauceCodePro Nerd Font, Courier Prime Code, Anka/Coder, Rec Mono Casual
+    {
+        weight = 'Regular',
+        italic = false,
+    }
 )
 
+-- config.harfbuzz_features = { 'zero' }
 
 config.tab_bar_at_bottom = true
 config.freetype_load_target = 'HorizontalLcd'
