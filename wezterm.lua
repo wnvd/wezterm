a68d650137709b26af35f525f3e8bd402d841595
naveed <waninaveed21@gmail.com>
2025-08-20 14:53:24 +0530

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,10 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+-- check for updates
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+
 --  Now here applying your configuration
 
 -- scroll keys
@@ -15,17 +19,21 @@ config.keys = {
     { key = 'UpArrow',   mods = 'SHIFT', action = act.ScrollByLine(-5) },
     { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollByLine(5) },
 }
+config.max_fps = 60
+-- config.color_scheme = 'Muse (terminal.sexy)'
 -- colorscheme (goes well with gruvbox 'trasparent mode = true')
 -- config.color_scheme = 'Oxocarbon Dark (Gogh)'
+config.color_scheme = 'Adventure'
 config.colors = {
     -- background = "#0a0d0f", -- subtle black
     cursor_bg = '#FFFF00',
     cursor_fg = 'black',
     cursor_border = '#FFFF00',
 }
+
 config.underline_thickness = '2.0'
 
-config.initial_cols = 70
+config.initial_cols = 80
 config.initial_rows = 20
 
 config.line_height = 1.0 -- below 1.0 makes some font smuggy
@@ -44,29 +52,52 @@ config.window_padding = {
     top = 0,
     bottom = 0,
 }
-config.font_size = 16.0
+config.font_size = 13.0
 config.cell_width = 1.00
 
 -- Some 'mono' fonts make icons in terminal look smaller.
 config.font = wezterm.font(
-    'Debugger',
+    -- 'Debugger Nerd Font',
+    -- 'Victor Mono Nerd Font',
+    -- 'DankMono Nerd Font',
+    'MapleMono NF',
+    -- 'JetBrainsMono Nerd Font',
     {
         weight = 'Regular',
         italic = false,
     }
 )
 
+
 config.harfbuzz_features = {
     -- universal ligatures on/off
-    'calt=0',
-    'clig=0',
-    'liga=0',
-    'zero',
+    'calt=1',
+    'clig=1',
+    'liga=1',
+    -- 'zero',
+    -- 'cv14',
+    -- Maple Mono
+    'cv01', -- Maple Mono: # $ & % ->
+    -- 'cv04', -- l
+    'calt', -- Maple Mono: connected cursive
+    'cv02', -- Maple Mono: a 
+    'cv35', -- Maple Mono: l is cursive
+    'cv09', -- Maple Mono: 7
+    'cv10', -- Maple Mono: ZZ, JetBrains Mono: g,t
+    'cv31', -- Maple Mono: a
+    'cv65', -- Maple Mono: &
+    -- Stylistic Sets
+    'ss11', -- Maple Mono: |=
 }
 
 config.default_workspace = "home"
 
+-- does not notify if font does not support "some" glyph
+config.warn_about_missing_glyphs = false
+
 config.tab_bar_at_bottom = true
-config.freetype_load_target = 'HorizontalLcd'
+config.freetype_load_target = 'Normal'
+config.freetype_render_target = 'HorizontalLcd'
+config.freetype_load_flags = 'NO_HINTING' -- this does somthing to fonts
 -- and finally, return the configuration to wezterm
 return config
